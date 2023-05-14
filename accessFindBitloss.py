d4b160ddf1f2bed048a0225d2b427ac2a1ac24f7
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2023-05-14 14:15:48 +0700

    first init

---
+++ accessFindBitloss.py
@@ -0,0 +1,74 @@
+'''
+09/08/2022
+Pham Minh Nhat
+PNM3HC
+Find Factor & Norm, chech bitloss
+'''
+
+
+
+import time
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font
+import os
+import numpy as np
+
+
+start_time = time.time()
+cwd = os.getcwd()
+
+def Find_file_Name(dir,keyname):
+    fileNames = os.listdir(dir)
+    for fileName in fileNames:
+        if(keyname in fileName):
+            file_path = os.path.abspath(os.path.join(dir, fileName))
+            file_name = fileName
+
+    return file_name
+
+def Find_col(keyname,row_max,col_max_num):
+    col_return = 0
+    row_return = 0
+    row_s = row_max
+    col_s = col_max_num
+    print(row_s,col_s)
+    for row in range(1,row_max+1):
+        for col in range(1,col_max_num+1):
+            col_char = get_column_letter(col)
+            print(col_char)
+            print(str(wsl[col_char +str(row)].value))
+            print(wsl[col_char +str(row)].value)
+            if keyname in str(wsl[col_char +str(row)].value):
+                col_return = col
+                row_return = row
+                print(col_return,row_return)
+    return col_return,row_return
+
+
+Ramcell_dir = Find_file_Name(cwd,'Ramcell')
+print('Accessing '+str(Ramcell_dir))
+wbl = load_workbook(Ramcell_dir)
+wsl = wbl.active
+print('accessing: '+ str(wsl))
+
+for row_count in range(1,1000):                                 # Count max rows and max cols 
+    for j in range(1,1000):
+        col_count = get_column_letter(j)
+        if not wsl[col_count + str(row_count)].value == None:
+            row_max = row_count
+            col_max = col_count
+            col_max_num = j
+print('active rows: '+str(row_max))
+print('active cols: '+str(col_max)+' : '+str(col_max_num))
+
+DID_col,DID_row_1st = Find_col('DID',row_max,col_max_num)
+print(DID_col)
+print(DID_row_1st)
+
+
+
+        
+
+end_time = time.time()
+print(str(end_time-start_time)+' s')
---
+++ dirpath.py
@@ -0,0 +1,23 @@
+import openpyxl
+import os
+import time
+
+start_time = time.time()
+cwd = os.getcwd()
+support_file='1'
+
+def listDir(dir,keyname):
+    fileNames = os.listdir(dir)
+    for fileName in fileNames:
+        if(keyname in fileName):
+            file_path = format(os.path.join(dir, fileName))
+            file_name = fileName
+    return file_name
+
+
+with open('test1.txt','w') as f:
+    support_file = listDir(cwd,'Support')
+    f.write(support_file+'\n')
+    f.write(cwd)
+    end_time = time.time()
+    f.write('\nExecution time: '+str(end_time-start_time)+' s')
